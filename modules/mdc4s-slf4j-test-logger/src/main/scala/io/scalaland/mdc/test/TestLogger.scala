8c8a7e59053173ed08c16895680544b1a6ef11a4
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2024-08-23 16:03:39 +0200

    Fix 2.12 and 3 implementation

--- modules/mdc4s-slf4j-test-logger/src/main/scala/io/scalaland/mdc/test/TestLogger.scala
+++ modules/mdc4s-slf4j-test-logger/src/main/scala/io/scalaland/mdc/test/TestLogger.scala
@@ -5,7 +5,6 @@ import org.slf4j.event.Level.*
 import org.slf4j.{Logger, Marker, MDC}
 
 import scala.annotation.nowarn
-import scala.collection.convert.ImplicitConversions.*
 
 class TestLogger(name: String) extends Logger {
 
@@ -15,8 +14,9 @@ class TestLogger(name: String) extends Logger {
 
   @nowarn
   private def log(level: Level, format: String, throwable: Option[Throwable] = None, marker: Option[Marker] = None)(
-      args: Any*
+      args: AnyRef*
   ) = synchronized {
+    import scala.collection.convert.ImplicitConversions.*
     logs = logs :+ TestLogger.Entry(
       level = level,
       message = if (args.isEmpty) format else format.format(args*),
@@ -31,63 +31,63 @@ class TestLogger(name: String) extends Logger {
   // format: off
   override def isTraceEnabled: Boolean = level.compareTo(TRACE) <= 0
   override def trace(msg: String): Unit = log(TRACE, msg)()
-  override def trace(format: String, arg: Any): Unit = log(TRACE, format)(arg)
-  override def trace(format: String, arg1: Any, arg2: Any): Unit = log(TRACE, format)(arg1, arg2)
-  override def trace(format: String, arguments: Any*): Unit = log(TRACE, format)(arguments*)
+  override def trace(format: String, arg: AnyRef): Unit = log(TRACE, format)(arg)
+  override def trace(format: String, arg1: AnyRef, arg2: AnyRef): Unit = log(TRACE, format)(arg1, arg2)
+  override def trace(format: String, arguments: AnyRef*): Unit = log(TRACE, format)(arguments*)
   override def trace(msg: String, t: Throwable): Unit = log(TRACE, msg, throwable = Some(t))()
   override def isTraceEnabled(marker: Marker): Boolean = level.compareTo(TRACE) <= 0
   override def trace(marker: Marker, msg: String): Unit = log(TRACE, msg, marker = Some(marker))()
-  override def trace(marker: Marker, format: String, arg: Any): Unit = log(TRACE, format)(arg)
-  override def trace(marker: Marker, format: String, arg1: Any, arg2: Any): Unit = log(TRACE, format, marker = Some(marker))(arg1, arg2)
-  override def trace(marker: Marker, format: String, arguments: Any*): Unit = log(TRACE, format, marker = Some(marker))(arguments*)
+  override def trace(marker: Marker, format: String, arg: AnyRef): Unit = log(TRACE, format)(arg)
+  override def trace(marker: Marker, format: String, arg1: AnyRef, arg2: AnyRef): Unit = log(TRACE, format, marker = Some(marker))(arg1, arg2)
+  override def trace(marker: Marker, format: String, arguments: AnyRef*): Unit = log(TRACE, format, marker = Some(marker))(arguments*)
   override def trace(marker: Marker, msg: String, t: Throwable): Unit = log(TRACE, msg, throwable = Some(t), marker = Some(marker))()
   override def isDebugEnabled: Boolean = level.compareTo(DEBUG) <= 0
   override def debug(msg: String): Unit = log(DEBUG, msg)()
-  override def debug(format: String, arg: Any): Unit = log(DEBUG, format)(arg)
-  override def debug(format: String, arg1: Any, arg2: Any): Unit = log(DEBUG, format)(arg1, arg2)
-  override def debug(format: String, arguments: Any*): Unit = log(DEBUG, format)(arguments*)
+  override def debug(format: String, arg: AnyRef): Unit = log(DEBUG, format)(arg)
+  override def debug(format: String, arg1: AnyRef, arg2: AnyRef): Unit = log(DEBUG, format)(arg1, arg2)
+  override def debug(format: String, arguments: AnyRef*): Unit = log(DEBUG, format)(arguments*)
   override def debug(msg: String, t: Throwable): Unit = log(DEBUG, msg, throwable = Some(t))()
   override def isDebugEnabled(marker: Marker): Boolean = level.compareTo(DEBUG) <= 0
   override def debug(marker: Marker, msg: String): Unit = log(DEBUG, msg, marker = Some(marker))()
-  override def debug(marker: Marker, format: String, arg: Any): Unit = log(DEBUG, format)(arg)
-  override def debug(marker: Marker, format: String, arg1: Any, arg2: Any): Unit = log(DEBUG, format, marker = Some(marker))(arg1, arg2)
-  override def debug(marker: Marker, format: String, arguments: Any*): Unit = log(DEBUG, format, marker = Some(marker))(arguments*)
+  override def debug(marker: Marker, format: String, arg: AnyRef): Unit = log(DEBUG, format)(arg)
+  override def debug(marker: Marker, format: String, arg1: AnyRef, arg2: AnyRef): Unit = log(DEBUG, format, marker = Some(marker))(arg1, arg2)
+  override def debug(marker: Marker, format: String, arguments: AnyRef*): Unit = log(DEBUG, format, marker = Some(marker))(arguments*)
   override def debug(marker: Marker, msg: String, t: Throwable): Unit = log(DEBUG, msg, throwable = Some(t), marker = Some(marker))()
   override def isInfoEnabled: Boolean = level.compareTo(INFO) <= 0
   override def info(msg: String): Unit = log(INFO, msg)()
-  override def info(format: String, arg: Any): Unit = log(INFO, format)(arg)
-  override def info(format: String, arg1: Any, arg2: Any): Unit = log(INFO, format)(arg1, arg2)
-  override def info(format: String, arguments: Any*): Unit = log(INFO, format)(arguments*)
+  override def info(format: String, arg: AnyRef): Unit = log(INFO, format)(arg)
+  override def info(format: String, arg1: AnyRef, arg2: AnyRef): Unit = log(INFO, format)(arg1, arg2)
+  override def info(format: String, arguments: AnyRef*): Unit = log(INFO, format)(arguments*)
   override def info(msg: String, t: Throwable): Unit = log(INFO, msg, throwable = Some(t))()
   override def isInfoEnabled(marker: Marker): Boolean = level.compareTo(INFO) <= 0
   override def info(marker: Marker, msg: String): Unit = log(INFO, msg, marker = Some(marker))()
-  override def info(marker: Marker, format: String, arg: Any): Unit = log(INFO, format)(arg)
-  override def info(marker: Marker, format: String, arg1: Any, arg2: Any): Unit = log(INFO, format, marker = Some(marker))(arg1, arg2)
-  override def info(marker: Marker, format: String, arguments: Any*): Unit = log(INFO, format, marker = Some(marker))(arguments*)
+  override def info(marker: Marker, format: String, arg: AnyRef): Unit = log(INFO, format)(arg)
+  override def info(marker: Marker, format: String, arg1: AnyRef, arg2: AnyRef): Unit = log(INFO, format, marker = Some(marker))(arg1, arg2)
+  override def info(marker: Marker, format: String, arguments: AnyRef*): Unit = log(INFO, format, marker = Some(marker))(arguments*)
   override def info(marker: Marker, msg: String, t: Throwable): Unit = log(INFO, msg, throwable = Some(t), marker = Some(marker))()
   override def isWarnEnabled: Boolean = level.compareTo(WARN) <= 0
   override def warn(msg: String): Unit = log(WARN, msg)()
-  override def warn(format: String, arg: Any): Unit = log(WARN, format)(arg)
-  override def warn(format: String, arg1: Any, arg2: Any): Unit = log(WARN, format)(arg1, arg2)
-  override def warn(format: String, arguments: Any*): Unit = log(WARN, format)(arguments*)
+  override def warn(format: String, arg: AnyRef): Unit = log(WARN, format)(arg)
+  override def warn(format: String, arg1: AnyRef, arg2: AnyRef): Unit = log(WARN, format)(arg1, arg2)
+  override def warn(format: String, arguments: AnyRef*): Unit = log(WARN, format)(arguments*)
   override def warn(msg: String, t: Throwable): Unit = log(WARN, msg, throwable = Some(t))()
   override def isWarnEnabled(marker: Marker): Boolean = level.compareTo(WARN) <= 0
   override def warn(marker: Marker, msg: String): Unit = log(WARN, msg, marker = Some(marker))()
-  override def warn(marker: Marker, format: String, arg: Any): Unit = log(WARN, format)(arg)
-  override def warn(marker: Marker, format: String, arg1: Any, arg2: Any): Unit = log(WARN, format, marker = Some(marker))(arg1, arg2)
-  override def warn(marker: Marker, format: String, arguments: Any*): Unit = log(WARN, format, marker = Some(marker))(arguments*)
+  override def warn(marker: Marker, format: String, arg: AnyRef): Unit = log(WARN, format)(arg)
+  override def warn(marker: Marker, format: String, arg1: AnyRef, arg2: AnyRef): Unit = log(WARN, format, marker = Some(marker))(arg1, arg2)
+  override def warn(marker: Marker, format: String, arguments: AnyRef*): Unit = log(WARN, format, marker = Some(marker))(arguments*)
   override def warn(marker: Marker, msg: String, t: Throwable): Unit = log(WARN, msg, throwable = Some(t), marker = Some(marker))()
   override def isErrorEnabled: Boolean = level.compareTo(ERROR) <= 0
   override def error(msg: String): Unit = log(ERROR, msg)()
-  override def error(format: String, arg: Any): Unit = log(ERROR, format)(arg)
-  override def error(format: String, arg1: Any, arg2: Any): Unit = log(ERROR, format)(arg1, arg2)
-  override def error(format: String, arguments: Any*): Unit = log(ERROR, format)(arguments*)
+  override def error(format: String, arg: AnyRef): Unit = log(ERROR, format)(arg)
+  override def error(format: String, arg1: AnyRef, arg2: AnyRef): Unit = log(ERROR, format)(arg1, arg2)
+  override def error(format: String, arguments: AnyRef*): Unit = log(ERROR, format)(arguments*)
   override def error(msg: String, t: Throwable): Unit = log(ERROR, msg, throwable = Some(t))()
   override def isErrorEnabled(marker: Marker): Boolean = level.compareTo(ERROR) <= 0
   override def error(marker: Marker, msg: String): Unit = log(ERROR, msg, marker = Some(marker))()
-  override def error(marker: Marker, format: String, arg: Any): Unit = log(ERROR, format)(arg)
-  override def error(marker: Marker, format: String, arg1: Any, arg2: Any): Unit = log(ERROR, format, marker = Some(marker))(arg1, arg2)
-  override def error(marker: Marker, format: String, arguments: Any*): Unit = log(ERROR, format, marker = Some(marker))(arguments*)
+  override def error(marker: Marker, format: String, arg: AnyRef): Unit = log(ERROR, format)(arg)
+  override def error(marker: Marker, format: String, arg1: AnyRef, arg2: AnyRef): Unit = log(ERROR, format, marker = Some(marker))(arg1, arg2)
+  override def error(marker: Marker, format: String, arguments: AnyRef*): Unit = log(ERROR, format, marker = Some(marker))(arguments*)
   override def error(marker: Marker, msg: String, t: Throwable): Unit = log(ERROR, msg, throwable = Some(t), marker = Some(marker))()
   // format: on
 }
